bb9533baa244acf3e0abb6bd93cc3a5a5799d750
efuaboy11 <iseghohimhene@gmail.com>
2025-12-11 01:03:12 +0100

    changes

--- backend_project/backend_project/settings.py
+++ backend_project/backend_project/settings.py
@@ -195,23 +195,23 @@ AUTH_USER_MODEL = 'base.NewUser'
 
 # EMAIL 
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = 'smtp.hostinger.com'
-# EMAIL_PORT = 465
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'support@veloxarion.com'
-# EMAIL_HOST_PASSWORD = '@Fintan123'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.hostinger.com'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iamterrysilva123456789@gmail.com'  # Your Gmail account
-EMAIL_HOST_PASSWORD = 'enio zatp intb hwtu'  # Your Gmail password or app-specific password
+EMAIL_HOST_USER = 'support@veloxarion.com'
+EMAIL_HOST_PASSWORD = '@Fintan123'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'iamterrysilva123456789@gmail.com'  # Your Gmail account
+# EMAIL_HOST_PASSWORD = 'enio zatp intb hwtu'  # Your Gmail password or app-specific password
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
